d04214f6185314ef86784d2e9e02e90c8bf0e5da
Li Tian <ben5green@outlook.com>
2021-04-28 12:26:17 +0800

    implemented order history screen

--- client/src/App.js
+++ client/src/App.js
@@ -12,6 +12,7 @@ import ShippingAddressScreen from './screens/ShippingAddressScreen';
 import PaymentMethodScreen from './screens/PaymentMethodScreen';
 import PlaceOrderScreen from './screens/PlaceOrderScreen';
 import OrderScreen from './screens/OrderScreen';
+import OrderHistoryScreen from './screens/OrderHistoryScreen';
 
 function App() {
     const cart = useSelector((state) => state.cart);
@@ -50,12 +51,19 @@ function App() {
                                     <i className="fa fa-caret-down"></i>{' '}
                                 </Link>
                                 <ul className="dropdown-content">
-                                    <Link
-                                        to="#signout"
-                                        onClick={signoutHandler}
-                                    >
-                                        Sign Out
-                                    </Link>
+                                    <li>
+                                        <Link to="/orderhistory">
+                                            Order History
+                                        </Link>
+                                    </li>
+                                    <li>
+                                        <Link
+                                            to="#signout"
+                                            onClick={signoutHandler}
+                                        >
+                                            Sign Out
+                                        </Link>
+                                    </li>
                                 </ul>
                             </div>
                         ) : (
@@ -73,6 +81,10 @@ function App() {
                     <Route path="/payment" component={PaymentMethodScreen} />
                     <Route path="/placeorder" component={PlaceOrderScreen} />
                     <Route path="/order/:id" component={OrderScreen} />
+                    <Route
+                        path="/orderhistory"
+                        component={OrderHistoryScreen}
+                    />
                 </main>
                 <footer className="row center">All Right Reserved</footer>
             </div>
--- client/src/actions/orderActions.js
+++ client/src/actions/orderActions.js
@@ -8,7 +8,9 @@ import {
     ORDER_PAY_REQUEST,
     ORDER_PAY_SUCCESS,
     ORDER_PAY_FAIL,
-    ORDER_PAY_RESET,
+    ORDER_MINE_LIST_REQUEST,
+    ORDER_MINE_LIST_FAIL,
+    ORDER_MINE_LIST_SUCCESS,
 } from '../constants/orderConstants';
 import Axios from 'axios';
 import { CART_EMPTY } from '../constants/cartConstant';
@@ -77,6 +79,29 @@ export const payOrder = (order, paymentResult) => async (
     } catch (error) {
         dispatch({
             type: ORDER_PAY_FAIL,
+            payload:
+                error.response && error.response.data.message
+                    ? error.response.data.message
+                    : error.message,
+        });
+    }
+};
+
+export const listOrderMine = () => async (dispatch, getState) => {
+    dispatch({ type: ORDER_MINE_LIST_REQUEST });
+    const {
+        userSignin: { userInfo },
+    } = getState();
+    try {
+        const { data } = await Axios.get('/api/orders/mine', {
+            headers: {
+                Authorization: `Baerer ${userInfo.token}`,
+            },
+        });
+        dispatch({ type: ORDER_MINE_LIST_SUCCESS, payload: data });
+    } catch (error) {
+        dispatch({
+            type: ORDER_MINE_LIST_FAIL,
             payload:
                 error.response && error.response.data.message
                     ? error.response.data.message
