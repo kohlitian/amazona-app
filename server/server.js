b3a156cac109e58b6016a90b997d8ab1f9dd20a1
Li Tian <ben5green@outlook.com>
2021-04-29 13:37:48 +0800

    serve all files and address to front end

--- server/server.js
+++ server/server.js
@@ -29,6 +29,9 @@ app.get('/api/config/paypal', (req, res) => {
     res.send(process.env.PAYPAL_CLIENT_ID || 'sb');
 });
 
+const __dirname = path.resolve();
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+
 app.use(express.static(path.join(__dirname, '/client/build')));
 app.get('*', (req, res) =>
     res.sendFile(path.join(__dirname, '/client/build/index.html'))
@@ -36,9 +39,6 @@ app.get('*', (req, res) =>
 // app.get('/', (req, res) => {
 //     res.send('Server is Ready');
 // });
-
-const __dirname = path.resolve();
-app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
 app.use((err, req, res, next) => {
     res.status(500).send({ message: err.message });
